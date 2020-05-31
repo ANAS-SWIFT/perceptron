9c4c158af25d1624b8fd242b45d4125ce956ee02
ANAS <anasmoumtaji98@gmail.com>
2020-05-31 00:30:32 +0000

    Initial Commit

---
+++ Perceptron/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Perceptron
+//
+//  Created by ANAS on 5/31/20.
+//  Copyright © 2020 ANAS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
